00ae0f8f366cf22d8aef52a5f494d528cdea15dc
raulAM7 <raulartilesm@gmail.com>
2025-01-09 19:01:47 +0000

    udpdate

--- code/Vendor_Raul/ModuloTest/registration.php
+++ code/Vendor_Raul/ModuloTest/registration.php
@@ -1,10 +1,11 @@
 <?php
 
 // Este es el archivo que registra en Magento el Modulo
+// Se ubica en app/code/Vendor/ModuleName -> la raiz del modulo
 
-use Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Component\ComponentRegistrar; // Usa la clase ComponentRegistrar nativa de Magento
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE, // Indica que se estra registrando un modulo
-     'Vendor_Raul_ModuloTest',  // 
-      __DIR__)
+     'Vendor_Raul_ModuloTest',  // Nombre del modulo y vendor al que pertenece
+      __DIR__); // Ubicacion del modulo en el sistema de archivos
